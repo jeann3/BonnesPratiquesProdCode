335c83ce78bc128e098bc824bded2b5c3c472ca0
Jeanne Corbeau <jcorbeau@biomaneo.intra>
2022-04-20 11:25:29 +0200

    telephone controller test 2

--- address-app/address-web/src/test/java/org/sam/mines/address/web/controller/TelephoneControllerTest.java
+++ address-app/address-web/src/test/java/org/sam/mines/address/web/controller/TelephoneControllerTest.java
@@ -1,26 +1,24 @@
 package org.sam.mines.address.web.controller;
 
 import org.junit.jupiter.api.Test;
-import org.sam.mines.address.model.TargetEntity;
-import org.sam.mines.address.model.TargetEntityBuilder;
 import org.sam.mines.address.model.TelephoneEntity;
-import org.sam.mines.address.model.TownEntity;
 import org.sam.mines.address.service.TelephoneEntityService;
 import org.sam.mines.address.web.config.WebTestConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -47,7 +45,7 @@ public class TelephoneControllerTest {
         when(telephoneEntityService.get(uuid)).thenReturn(Optional.of(
                 TelephoneEntity.TelephoneEntityBuilder.aTelephoneEntity()
                         .withId(uuid)
-                        .withNumber("010203040506")
+                        .withNumber("0102030405")
                         .withPrefix("33")
                         .build()
         ));
@@ -56,12 +54,19 @@ public class TelephoneControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/telephone/%s".formatted(uuid.toString()))
                         .accept(MediaType.ALL))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.number").value("010203040506"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.number").value("0102030405"));
 
 
     }
 
     @Test
-    void listTelephone() {
+    void listTelephone() throws Exception {
+        when(telephoneEntityService.getAll()).thenReturn(new ArrayList<>());
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/telephone"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        assertEquals(result.getResponse().getContentLength(), 0);
     }
 }
